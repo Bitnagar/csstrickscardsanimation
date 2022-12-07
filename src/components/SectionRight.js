1233a1fe6b8bc93f71a31b6d6037260d94160d3e
Bitnagar <shivambhatnagar30@gmail.com>
2022-12-07 20:24:24 +0530

    update dates

--- src/components/SectionRight.js
+++ src/components/SectionRight.js
@@ -4,21 +4,21 @@ import Card from './Card';
 export default function SectionRight() {
   const data = [
     {
-      date: 'Oct 01, 2022',
+      date: 'Nov 01, 2022',
       heading: 'Responsive Animations for Every Screen Size and Device',
       tags: ['animation', 'canvas', 'flip', 'gsap', 'svg animation'],
       picture: './pngegg.png',
       name: 'bitnagar',
     },
     {
-      date: 'Oct 05, 2022',
+      date: 'Nov 05, 2022',
       heading: 'How to Center a Div 101',
       tags: ['css mastery', 'expert', 'html', 'no js'],
       picture: './pngegg.png',
       name: 'bitnagar',
     },
     {
-      date: 'Oct 12, 2022',
+      date: 'Nov 12, 2022',
       heading: 'How to Build for the Mobile Web',
       tags: [
         'mobile web dev',
@@ -31,35 +31,35 @@ export default function SectionRight() {
       name: 'bitnagar',
     },
     {
-      date: 'Oct 18, 2022',
+      date: 'Nov 18, 2022',
       heading: 'Increase Developer Productivity with these Steps!',
       tags: ['productivity', 'hack', 'work', 'code', 'developer tools'],
       picture: './pngegg.png',
       name: 'bitnagar',
     },
     {
-      date: 'Oct 25, 2022',
+      date: 'Nov 25, 2022',
       heading: 'Must know JS Tricks 2022 Edition',
       tags: ['JavaScript', 'JS Mastery', 'animation', 'DOM'],
       picture: './pngegg.png',
       name: 'bitnagar',
     },
     {
-      date: 'Oct 01, 2022',
+      date: 'Nov 01, 2022',
       heading: 'Responsive Animations for Every Screen Size and Device',
       tags: ['animation', 'canvas', 'flip', 'gsap', 'svg animation'],
       picture: './pngegg.png',
       name: 'bitnagar',
     },
     {
-      date: 'Oct 05, 2022',
+      date: 'Nov 05, 2022',
       heading: 'How to Center a Div 101',
       tags: ['css mastery', 'expert', 'html', 'no js'],
       picture: './pngegg.png',
       name: 'bitnagar',
     },
     {
-      date: 'Oct 12, 2022',
+      date: 'Nov 12, 2022',
       heading: 'How to Build for the Mobile Web',
       tags: [
         'mobile web dev',
@@ -72,14 +72,14 @@ export default function SectionRight() {
       name: 'bitnagar',
     },
     {
-      date: 'Oct 18, 2022',
+      date: 'Nov 18, 2022',
       heading: 'Increase Developer Productivity with these Steps!',
       tags: ['productivity', 'hack', 'work', 'code', 'developer tools'],
       picture: './pngegg.png',
       name: 'bitnagar',
     },
     {
-      date: 'Oct 25, 2022',
+      date: 'Nov 25, 2022',
       heading: 'Must know JS Tricks 2022 Edition',
       tags: ['JavaScript', 'JS Mastery', 'animation', 'DOM'],
       picture: './pngegg.png',
