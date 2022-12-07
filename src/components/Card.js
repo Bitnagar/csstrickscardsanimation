47b26c13dc59a5901efdb37ba59341ba8c0cc0fe
Bitnagar <shivambhatnagar30@gmail.com>
2022-12-07 19:03:30 +0530

    add responsiveness

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import $ from 'jquery';
 
 export default function Card(props) {
   function handleMouseEnter(e) {
+    let md = 746;
+    let vw = window.innerWidth;
     const allCards = $('.card-holder').children();
     e.preventDefault();
     $(allCards[props.pos])
       .nextAll()
       .each(function () {
-        if (window.innerWidth > 500) {
+        if (vw > md) {
           $(this).css('transform', 'translateX(10rem)');
         } else {
           $(this).css('transform', 'translateX(2rem)');
@@ -35,19 +38,11 @@ export default function Card(props) {
         <p>
           <b>Article</b> <span>on {props.data.date}</span>
         </p>
-        <a href="https://www.example.com" target="_blank" rel="noreferrer">
-          {props.data.heading}
-        </a>
+        <a href="#">{props.data.heading}</a>
         <div className="tags">
           {props.data.tags.map((tag, key) => {
             return (
-              <a
-                key={key}
-                href="https://www.example.com"
-                target="_blank"
-                rel="noreferrer"
-                id="tag"
-              >
+              <a key={key} href="#" id="tag">
                 {tag.toUpperCase()}
               </a>
             );
